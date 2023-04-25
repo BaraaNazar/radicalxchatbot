6f0a5480f72f765e90de5b0b442d4ad8fc97aa7e
Baraa-Nazar <baraa.nazar.ph@gmail.com>
2023-04-25 19:41:20 +0300

    main branch

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="h-screen w-screen flex justify-center items-center">
+
+    <iframe className="h-full w-fit" src="https://mediafiles.botpress.cloud/bcfdea2b-cb3d-4ff1-a3fb-a554ea088e5f/webchat/bot.html"></iframe>
+  </div>
+  );
+}
+
+export default App;
