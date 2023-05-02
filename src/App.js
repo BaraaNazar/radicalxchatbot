11bdbc4d8972065869116103cc5303500bbe2d20
Baraa-Nazar <baraa.nazar.ph@gmail.com>
2023-05-02 17:02:25 +0300

    Wide version of the chat

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
 import React, { useEffect } from 'react';
-import './App.css';
 function App() {
   useEffect(()=>{
-  window.botpressWebChat.init({
-      botId: "bcfdea2b-cb3d-4ff1-a3fb-a554ea088e5f",
-      hostUrl: "https://cdn.botpress.cloud/webchat/v0",
-      messagingUrl: "https://messaging.botpress.cloud",
-      clientId: "bcfdea2b-cb3d-4ff1-a3fb-a554ea088e5f",
-      showTimestamp: "true",
-      showConversationsButton: "true",
-      enableConversationDeletion: "true",
-      botName:"RadicalX",
-      botConversationDescription:"Front end lead",
-      coverPictureUrl:"https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%2Fradicalxpinterest%2F&psig=AOvVaw3FxKYN-W1wOzB91EO2Qdcf&ust=1682686744644000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCIDYzt-Oyv4CFQAAAAAdAAAAABAI"
+      window.botpressWebChat.init({
+          botId: "bcfdea2b-cb3d-4ff1-a3fb-a554ea088e5f",
+          hostUrl: "https://cdn.botpress.cloud/webchat/v0",
+          messagingUrl: "https://messaging.botpress.cloud",
+          clientId: "bcfdea2b-cb3d-4ff1-a3fb-a554ea088e5f",
+          showTimestamp: "true",
+          showConversationsButton: "true",
+          enableConversationDeletion: "true",
+          botName:"RadicalX",
+          botConversationDescription:"Front end lead",
+          stylesheet:"file:///Users/baraanazar/Desktop/untitled%20folder/App.css",
+          showBotInfoPage: "true",
+          containerWidth: "100%25",
+          layoutWidth: "100%25",
   });
+const scrollContainer = document.getElementsByClassName("bpw-floating-button")
+scrollContainer.style.backgroundColor = "blue";
+scrollContainer.style.fontSize = "20px"
+scrollContainer.style.width = "200px"
   },[])
   return (
-   <div className='h-screen w-screen flex justify-center items-center'>
+   <div>
    </div>
   );
 }
